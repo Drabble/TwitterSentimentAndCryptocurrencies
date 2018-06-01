3c0506b6217fadf52d790029449125f516759360
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2018-06-01 12:55:15 +0200

    print streaming in console to show evolution

--- streamer/stream_crypto.js
+++ streamer/stream_crypto.js
@@ -18,7 +18,7 @@ socket.on("m", function(message) {
         var price = table[5];
         //var now = moment().format("YYYY-MM-DD HH:mm:ss");
         var now = moment().add(-2, 'hours').format("YYYY-MM-DD HH:mm:ss");
-        //console.log(now, price);
+        console.log(now, price);
         var streamObject = JSON.stringify({ x : now, y : price });
         plotlystream.write(streamObject+'\n');
     }
